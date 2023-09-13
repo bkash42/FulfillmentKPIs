271874a18823c04b0f500e1bba6a62f7f2cf5ddf
Frank Coniglio <fchammer15@gmail.com>
2023-09-13 15:16:35 -0400

    removing env file

--- function_app.py
+++ function_app.py
@@ -7,8 +7,7 @@ from FulfillmentKPIsV1 import calculate_kpi
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True) 
 def TimerTrigger(myTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
