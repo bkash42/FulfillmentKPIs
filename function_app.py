919d4d27acc6a6532f3da457264aec29a1be7df2
Frank Coniglio <fchammer15@gmail.com>
2023-09-14 09:29:49 -0400

    name changes on decorator

--- function_app.py
+++ function_app.py
@@ -7,13 +7,13 @@ from FulfillmentKPIsV1 import calculate_kpi
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False) 
+@app.schedule(schedule="0 */5 * * * *", arg_name="test_timer", run_on_startup=False) 
 def test_function(test_timer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
     calculate_kpi()
 
-    if myTimer.past_due:
+    if test_timer.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
